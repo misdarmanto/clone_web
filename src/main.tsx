9c4bdc8bb67b4e310b6ead914432f6c92e577de3
misdarmanto <misdarmanto1922@gmail.com>
2025-06-16 11:48:34 +0700

    feat: create context provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import AppRouters from "./routers";
+import { AppProvider } from "./context/app.context";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AppRouters />
+    <AppProvider>
+      <AppRouters />
+    </AppProvider>
   </StrictMode>
 );
