a649ca1d10f23ff2ab2af5d16b8914732fe87519
misdarmanto <misdarmanto1922@gmail.com>
2025-06-23 15:30:11 +0700

    feat: integrate detail dataset page into api

--- src/types/dataset.interface.ts
+++ src/types/dataset.interface.ts
@@ -5,3 +5,29 @@ export interface IDataset {
   modified: string;
   nama_opd: string;
 }
+
+interface InputData {
+  kode_dssd: string;
+  uraian_dssd: string;
+  tahun: number;
+  jumlah: number;
+}
+
+export interface IDatasetDetail {
+  id: number;
+  id_opd: number;
+  title: string;
+  description: string;
+  nama_opd: string;
+  kode_urusan: string;
+  jenis: number;
+  kategori: number;
+  jenis_string: string;
+  kategori_string: string;
+  kode_dssd: string;
+  uraian_dssd: string;
+  satuan: string;
+  dimensi: string;
+  input: InputData[];
+  download_url: string;
+}
